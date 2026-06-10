2f80ab20fa801d9d24240d7dc405c4abb86957ad
Soniya Vinaykumar Kambli <kamblisoniya@gmail.com>
2026-06-10 22:05:21 +0530

    Leetcode

--- SQL/1327.sql
+++ SQL/1327.sql
@@ -98,7 +98,7 @@ SELECT
 FROM Products P
 JOIN Orders O
 ON O.product_id=P.product_id
-WHERE O.order_date BETWEEN '2020-02-01' AND '2020-02-28'
+WHERE O.order_date BETWEEN '2020-02-01' AND '2020-02-29'
 GROUP BY P.product_name
 HAVING SUM(O.unit)>=100;
 
