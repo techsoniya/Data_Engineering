b7dac5893dad25e1447c57cfa1e5f4c56c934dd8
Soniya Vinaykumar Kambli <kamblisoniya@gmail.com>
2026-06-12 08:47:09 +0530

    Leetcode

--- SQL/1517.sql
+++ SQL/1517.sql
@@ -62,6 +62,10 @@
 
 ---------------------------------------------------------------------------------------------------------------------------
 
--- SELECT *
--- FROM Users 
--- WHERE mail REGEXP '^[A-Za-z][A-Za-z0-9_.-]*@leetcode\.com$';
+SELECT *
+FROM Users
+WHERE REGEXP_LIKE(
+    mail,
+    '^[A-Za-z][A-Za-z0-9_.-]*@leetcode\\.com$',
+    'c'
+);
