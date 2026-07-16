03ae1d5a8ed6a4e78bf92c6d5dcdfa0d07ef7a4b
Soniya Vinaykumar Kambli <kamblisoniya@gmail.com>
2026-07-16 23:43:57 +0530

    Leetcode

--- SQL/550.sql
+++ SQL/550.sql
@@ -48,19 +48,19 @@
 -- Explanation: 
 -- Only the player with id 1 logged back in after the first day he had logged in so the answer is 1/3 = 0.33
 
-SELECT
-    ROUND(
-        COUNT(DISTINCT a.player_id) /
-        (SELECT COUNT(DISTINCT player_id) FROM Activity),
-        2
-    ) AS fraction
-FROM Activity a
-JOIN (
-    SELECT
-        player_id,
-        MIN(event_date) AS first_login
-    FROM Activity
-    GROUP BY player_id
-) f
-ON a.player_id = f.player_id
-WHERE a.event_date = DATE_ADD(f.first_login, INTERVAL 1 DAY);
+-- SELECT
+--     ROUND(
+--         COUNT(DISTINCT a.player_id) /
+--         (SELECT COUNT(DISTINCT player_id) FROM Activity),
+--         2
+--     ) AS fraction
+-- FROM Activity a
+-- JOIN (
+--     SELECT
+--         player_id,
+--         MIN(event_date) AS first_login
+--     FROM Activity
+--     GROUP BY player_id
+-- ) f
+-- ON a.player_id = f.player_id
+-- WHERE a.event_date = DATE_ADD(f.first_login, INTERVAL 1 DAY);
